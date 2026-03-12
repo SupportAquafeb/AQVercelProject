b1a5522771f761152f8e91fe635791a7bd79591c
support@aquafeb.com <223996272+SupportAquafeb@users.noreply.github.com>
2026-03-12 16:26:39 +0800

    圖片

--- new.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-git config --global user.email "223996272+SupportAquafeb@users.noreply.github.com"
-git config --global user.name "support@aquafeb.com"
-git commit --amend --reset-author
-git push -u origin main
-
-pause
